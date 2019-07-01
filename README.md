8897438e12ba39dcdb79e9c69adbc1c5ae2056ad
Tran Hoang <tran.hoangnguyen@asnet.com.vn>
2019-07-01 17:16:23 +0700

    Algolia search example.

---
+++ README.md
@@ -0,0 +1,29 @@
+# Algolia Example
+
+## Prerequisite
+
+Node version: 8.10
+
+## Install dependencies
+
+``` bash
+npm install
+```
+
+## Upload contact to Algolia Index
+
+``` bash
+npm run upload-contacts
+```
+
+## Update search setting
+
+``` bash
+npm run search-settings
+```
+
+## Search
+
+``` bash
+npm run search
+```
---
+++ index.js
@@ -0,0 +1,52 @@
+const algoliasearch = require('algoliasearch');
+const client = algoliasearch('K0HE7D0AWF', '8d06610e21bc1a017513ed08a85f9b6b');
+const index = client.initIndex('dev_ALGOLIA');
+
+const contactsJSON = require('./contacts.json');
+
+module.exports.uploadContacts = function() {
+  index.addObjects(contactsJSON, (err, content) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("The contacts.json is uploaded to dev_ALGOLIA indices.")
+    }
+  });
+};
+
+module.exports.searchSettings = function() {
+  index.setSettings({
+    'searchableAttributes': [
+      'lastname',
+      'firstname',
+      'company',
+      'email',
+      'city',
+      'address'
+    ]
+  }, (err, content) => {
+    console.log(content);
+  });
+}
+
+module.exports.search = function() {
+  // Search for a first name
+  index.search('jimmie', (err, { hits } = {}) => {
+    console.log(hits);
+  });
+
+  // Search for a first name with typo
+  index.search('jimie', (err, { hits } = {}) => {
+    console.log(hits);
+  });
+
+  // Search for a company
+  index.search('california paint', (err, { hits } = {}) => {
+    console.log(hits);
+  });
+
+  // Search for a first name and a company
+  index.search('jimmie paint', (err, { hits } = {}) => {
+    console.log(hits);
+  });
+};
